144a974ce5fe59c917206b100c80e19d7d7689ca
dasterer <141071038+frogen-while@users.noreply.github.com>
2026-03-20 17:28:40 +0100

    feat(settings): add staticfiles storage configuration for improved asset management

--- vol_registr/settings.py
+++ vol_registr/settings.py
@@ -144,6 +144,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
